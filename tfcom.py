da16786229cd31be3d77e2e6cf0edf9ffeef2968
Geethaka Shyamal Perera <geethakashyamal@gmail.com>
2023-09-07 20:05:21 +0530

    Added apply and destroy

--- tfcom.py
+++ tfcom.py
@@ -2,7 +2,6 @@ import os
 import subprocess
 from user import nameEntProject
 
-
 def exec_tf(command, project_folder, command_name):
     print(f"\033[0;34mRunning '{command_name}' in {project_folder}...\033[0m")
 
@@ -24,7 +23,6 @@ def exec_tf(command, project_folder, command_name):
         print(f"\033[91mError while executing '{command_name}' in {project_folder}\033[0m")
         print(stderr)
 
-
 def tf_commands(base_folder):
     project_folders = [
         folder for folder in os.listdir(base_folder) if os.path.isdir(os.path.join(base_folder, folder))
@@ -37,18 +35,13 @@ def tf_commands(base_folder):
         exec_tf(init_command, project_path, "terraform init")
 
         config_plan_command = ["terraform", "plan", "-generate-config-out=generated_output.tf"]
-        exec_tf(config_plan_command, project_path, "terraform -config plan")
+        exec_tf(config_plan_command, project_path, "terraform plan")
 
-        # The following commands are commented out because they are not needed in this case.
-        # plan_command = ["terraform", "plan"]
-        # exec_tf(plan_command, project_path, "terraform plan")
-        #
-        # apply_command = ["terraform", "apply", "-auto-approve"]
-        # exec_tf(apply_command, project_folder, "terraform apply")
-        #
-        # destroy_command = ["terraform", "destroy", "-auto-approve"]
-        # exec_tf(destroy_command, project_folder, "terraform destroy")
+        apply_command = ["terraform", "apply", "-auto-approve"]
+        exec_tf(apply_command, project_path, "terraform apply")
 
+        destroy_command = ["terraform", "destroy", "-auto-approve"]
+        exec_tf(destroy_command, project_path, "terraform destroy")
 
 if __name__ == "__main__":
     base_folder = "tf_configs"
